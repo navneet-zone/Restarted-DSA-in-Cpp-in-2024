58e5307f24e070d6d0b9e7be214a0f226ebc9ea2
NAVNEET SINGH BHAKUNI <105786106+navneet-zone@users.noreply.github.com>
2024-03-10 17:20:58 +0530

    update

---
+++ patterns/45.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n=5;
+	for (int i = 5; i>=1; i--)
+	{
+		for (int j = 5; j>=1; j--)
+		{
+			if (i>=j)
+				cout<<"*";
+			else
+			cout<<" ";
+		}
+	cout<<endl;
+}
+	return 0;
+}
---
+++ patterns/55.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n=4;
+	int z=1;
+	for (int i = 1; i <= n; i++)
+	{
+		for(int j=n-1;j>=i;j--){
+			cout<<" ";
+		}
+		for(int k = 1; k<=z;k++){
+			cout<<i;
+		}
+		z+=2;
+		cout<<endl;
+	}
+}
