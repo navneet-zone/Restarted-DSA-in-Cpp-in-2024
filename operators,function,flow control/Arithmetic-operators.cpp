4957a43b05a72f324d83f8d7318e1d01f97fa2a8
NAVNEET SINGH BHAKUNI <105786106+navneet-zone@users.noreply.github.com>
2024-03-14 23:25:07 +0530

    update

--- operators,function,flow control/Arithmetic-operators.cpp
+++ operators,function,flow control/Arithmetic-operators.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int a = 232;
-	int b = 789;
+	int a ;
+	cout<<"enter value of a";
+	cin >>a; 
+	int b ;
+	cout<<"enter value of b";
+	cin >>b; 
 	cout << "a + b = " << (a + b) << endl;
 	cout << "a - b = " << (a - b) << endl;
 	cout << "a * b = " << (a * b) << endl;
